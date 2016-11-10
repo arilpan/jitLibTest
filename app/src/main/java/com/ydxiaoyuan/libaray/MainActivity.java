516e1d527941e8be93862f35bd8866ca65c5bf32
arilpan <arilpan@qq.com>
2016-11-10 09:55:01 +0800

    use it succeess

--- app/src/main/java/com/ydxiaoyuan/libaray/MainActivity.java
+++ app/src/main/java/com/ydxiaoyuan/libaray/MainActivity.java
@@ -1,5 +1,6 @@
 package com.ydxiaoyuan.libaray;
 
+import com.ydxiaoyuan.lib.StringUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+    StringUtil.output("test string");
   }
 }
