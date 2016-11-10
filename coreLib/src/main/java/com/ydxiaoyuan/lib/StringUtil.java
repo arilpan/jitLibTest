7960f11515511966860db745e0e93649992007a0
arilpan <arilpan@qq.com>
2016-11-10 09:42:48 +0800

    first commit

---
+++ coreLib/src/main/java/com/ydxiaoyuan/lib/StringUtil.java
@@ -0,0 +1,13 @@
+package com.ydxiaoyuan.lib;
+
+import android.util.Log;
+
+/**
+ * Created by arilpan@qq.com on 2016/11/10 in project MyApplication4.
+ * TODO: Add a class header comment!
+ */
+public class StringUtil {
+  public static void output(String string) {
+    Log.e("arilpan", "recv :" + string);
+  }
+}
